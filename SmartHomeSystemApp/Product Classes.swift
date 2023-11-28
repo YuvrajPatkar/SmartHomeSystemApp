609bba8be0f091a579ce51444ad792b490ac9df2
Yuvraj Patkar <patkar.y@etoncollege.org.uk>
2023-11-28 10:33:24 +0000

    halfway throug double div progress

--- SmartHomeSystemApp/Product Classes.swift
+++ SmartHomeSystemApp/Product Classes.swift
@@ -7,13 +7,43 @@
 
 import Foundation
 
-class Sonos {
+
+class Sonos{
+    var model = String
+    var color = String
+    var volume = Int
+    var playing = Bool
+
+    init(model: String = "Sonos", color: String = "black", volume: Int = 5, playing: Bool = true) {
+        self.model = model
+        self.color = color
+        self.volume = volume
+        self.playing = playing
+    }
     
+    func pause (){
+        playing = false
+    }
     
+    func unpause(){
+        playing = true
+    }
     
+    func increaseVolume(){
+        volume += 1
+    }
     
+    func decreaseVolume(){
+        volume -= 1
+    }
 }
 
-class SonosSoundBar: Sonos {
-    var color =
+class smartBulb{
+// rgb values from 0 - 255
+    var red = 0
+    var green = 0
+    var blue = 0
+    
+    var brightness = 0.0
+    
 }
--- SmartHomeSystemApp/main.swift
+++ SmartHomeSystemApp/main.swift
@@ -7,5 +7,14 @@
 
 import Foundation
 
-print("Hello, World!")
+func homeScreen() {
+    print("Welcome to your smart home system!")
+    print("To add a new device, enter 1")
+    print("To view your current devices, enter 2")
+          
+    let userInput = readLine()
+    if userInput == "1" {
+        print("Are you adding a new Sonos?: ")
+    }
+}
 
